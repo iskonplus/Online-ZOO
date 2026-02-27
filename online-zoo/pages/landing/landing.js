88b6a754b9d59cc051aed4053b30e9f8647eba2b
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2026-02-27 12:18:24 +0100

    feat: add burger menu toggle functionality

--- online-zoo/pages/landing/landing.js
+++ online-zoo/pages/landing/landing.js
@@ -2,3 +2,11 @@ import { initSlider } from "/pages/helpers/slider.js";
 
 initSlider(document.querySelector(".meet"));
 initSlider(document.querySelector(".our-users"));
+
+const burgerBtn = document.querySelector('.burger');
+const menu = document.querySelector('.header ul');
+
+burgerBtn.addEventListener('click', () => {
+  burgerBtn.classList.toggle('active');
+  menu.classList.toggle('active');
+});
