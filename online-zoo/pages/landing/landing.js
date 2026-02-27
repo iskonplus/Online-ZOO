321ca0cc59371ee38ee5e5a34a5545d33e3d49c1
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2026-02-27 12:26:46 +0100

    feat: close mobile menu after link selection

--- online-zoo/pages/landing/landing.js
+++ online-zoo/pages/landing/landing.js
@@ -5,8 +5,13 @@ initSlider(document.querySelector(".our-users"));
 
 const burgerBtn = document.querySelector('.burger');
 const menu = document.querySelector('.header ul');
+const menuLink = document.querySelector('.header ul li');
 
-burgerBtn.addEventListener('click', () => {
-  burgerBtn.classList.toggle('active');
+burgerBtn.addEventListener('click', () => handlerBurgerBtn());
+
+function handlerBurgerBtn() {
+      burgerBtn.classList.toggle('active');
   menu.classList.toggle('active');
-});
+}
+
+menuLink.addEventListener('click', () => handlerBurgerBtn());
