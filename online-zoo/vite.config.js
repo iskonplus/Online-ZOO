ae0be58c17a1e005f96a5788b7f6e07d44ba8a53
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2026-03-05 23:29:55 +0100

    fix: bugs with Netlify

--- online-zoo/vite.config.js
+++ online-zoo/vite.config.js
@@ -6,8 +6,9 @@ export default defineConfig({
       input: {
         home: "index.html",
         landing: "pages/landing/index.html",
-        zoos: "pages/zoos/index.html",
+        zoos: "pages/zoos/zoos.html",
         map: "pages/animal/animal.html",
+        contact: "pages/contact/contact.html",
       },
     },
   },
