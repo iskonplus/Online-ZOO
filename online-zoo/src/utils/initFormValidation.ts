89a87468825676be3782bc3dece25ff25f351b53
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2026-03-10 19:54:42 +0100

    refactor: improve logic

--- online-zoo/src/utils/initFormValidation.ts
+++ online-zoo/src/utils/initFormValidation.ts
@@ -1,24 +1,17 @@
 import type { FieldConfig } from "../types/validator";
 
 export function initFormValidation(fields: FieldConfig[]): void {
-  fields.forEach(({ input, error, validator }) => {
-    input.addEventListener("blur", () => {
-      input.classList.add("invalid");
-      validate();
-    });
-
+  fields.forEach(({ input, error, validator }): void => {
+    input.addEventListener("blur", validate);
     input.addEventListener("input", validate);
 
     function validate(): void {
       const message = validator(input.value.trim());
+      input.classList.add("invalid");
 
-      if (message) {
-          input.classList.add("invalid");
-          error.textContent = message;
-    } else {
-        input.classList.remove("invalid");
-          error.textContent = "";
-      }
+      if (!message) input.classList.remove("invalid");
+      error.textContent = message;
     }
+      
   });
 }
